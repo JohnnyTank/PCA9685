c87b2b3cc35d7e149456da09138efd7b771666cf
JohnnyTank <37960269+JohnnyTank@users.noreply.github.com>
2022-12-10 15:11:16 +0100

    Update Servo_test.py
    Working Version

--- examples/Servo_test.py
+++ examples/Servo_test.py
@@ -1,7 +1,30 @@
-import PCA9685
-from machine import Pin, SoftI2C
+# https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
+#from servo import Servos
+from machine import Pin, I2C
+from servo import Servos
+from time import sleep_ms
 
-i2c = SoftI2C(scl=Pin(20), sda=Pin(21), freq=100000)
+# machine.I2C(id, *, scl, sda, freq=400000)
+# SCL -> Pin(21)
+# SDA -> Pin(20)
+ID = 0
+_scl = Pin(1)
+_sda = Pin(0)
+i2c = I2C( id=ID, scl=_scl, sda=_sda)
 adr_room = i2c.scan()
 for adr in adr_room:
     print(hex(adr))
+
+    
+servo_board = Servos(i2c=i2c)
+
+for servo in range(16):
+    servo_board.position(index=servo, degrees=0)
+
+sleep_ms(3000)
+for servo in range(16):
+    servo_board.position(index=servo, degrees=90)
+    
+sleep_ms(3000)
+for servo in range(16):
+    servo_board.position(index=servo, degrees=180)
